77cc45631b906dade316462031e1be5467e0a36d
serp <torbjorncla@gmail.com>
2014-11-03 11:52:04 +0100

    Resolved unit-test and added categorization

--- GetAggregatedImagingOutcome/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getaggregatedimagingoutcome/QueryObjectFactoryTest.java
+++ GetAggregatedImagingOutcome/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getaggregatedimagingoutcome/QueryObjectFactoryTest.java
@@ -1,24 +1,102 @@
 package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.getaggregatedimagingoutcome;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import java.util.UUID;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Ignore;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
-import se.skltp.agp.service.api.QueryObjectFactory;
+import riv.clinicalprocess.healthcond.actoutcome.getimagingoutcomeresponder.v1.GetImagingOutcomeType;
+import riv.clinicalprocess.healthcond.actoutcome.getimagingoutcomeresponder.v1.ObjectFactory;
+import riv.clinicalprocess.healthcond.actoutcome.v3.PersonIdType;
+import se.skltp.agp.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentType;
 
 
 public class QueryObjectFactoryTest {
 
-	private QueryObjectFactory testObject = new QueryObjectFactoryImpl();
+	private static final QueryObjectFactoryImpl testObject = new QueryObjectFactoryImpl();
+	private static final ObjectFactory objFactory = new ObjectFactory();
+	
+	private static final String CATEGORIZATION = UUID.randomUUID().toString();
+	private static final String SERVICE_DOMAIN = UUID.randomUUID().toString();
+	private static final String SUBJECTOFCARE = UUID.randomUUID().toString();
+	private static final String SOURCESYSTEMHSAID = UUID.randomUUID().toString();
+	
+	
+	@BeforeClass
+	public static void init() {
+		testObject.setEiCategorization(CATEGORIZATION);
+		testObject.setEiServiceDomain(SERVICE_DOMAIN);
+	}
 	
 	@Test
-	@Ignore
-	public void testQueryObjectFactory() {
+	public void testQueryObjectFactorySuccess() throws Exception {
+		final GetImagingOutcomeType type = new GetImagingOutcomeType();
 		
-		Node node = null;
-		testObject.createQueryObject(node);
-		assertEquals("expected", "actual");
+		final PersonIdType person = new PersonIdType();
+		person.setId(SUBJECTOFCARE);
+		type.setPatientId(person);
+		type.setSourceSystemHSAId(SOURCESYSTEMHSAID);
+		
+		final Node node = createNode(type);
+		final FindContentType findContent = testObject.createQueryObject(node).getFindContent();
+
+		assertEquals(CATEGORIZATION, findContent.getCategorization());
+		assertEquals(SERVICE_DOMAIN, findContent.getServiceDomain());
+		assertEquals(SUBJECTOFCARE, findContent.getRegisteredResidentIdentification());
+		assertEquals(SOURCESYSTEMHSAID, findContent.getLogicalAddress());
+		assertEquals(SOURCESYSTEMHSAID, findContent.getSourceSystem());
+		assertNull(findContent.getBusinessObjectInstanceIdentifier());
+		assertNull(findContent.getClinicalProcessInterestId());
+		assertNull(findContent.getDataController());
+		assertNull(findContent.getMostRecentContent());
+		assertNull(findContent.getOwner());
+	}
+	
+	@Test
+	public void testQueryObjectFactoryEmptySourceSystem() throws Exception {
+		final GetImagingOutcomeType type = new GetImagingOutcomeType();
+		
+		final PersonIdType person = new PersonIdType();
+		person.setId(SUBJECTOFCARE);
+		type.setPatientId(person);
+		type.setSourceSystemHSAId(null);
+		
+		final Node node = createNode(type);
+		final FindContentType findContent = testObject.createQueryObject(node).getFindContent();
+		
+		assertEquals(CATEGORIZATION, findContent.getCategorization());
+		assertEquals(SERVICE_DOMAIN, findContent.getServiceDomain());
+		assertEquals(SUBJECTOFCARE, findContent.getRegisteredResidentIdentification());
+		assertNull(findContent.getLogicalAddress());
+		assertNull(findContent.getSourceSystem());
+		assertNull(findContent.getBusinessObjectInstanceIdentifier());
+		assertNull(findContent.getClinicalProcessInterestId());
+		assertNull(findContent.getDataController());
+		assertNull(findContent.getMostRecentContent());
+		assertNull(findContent.getOwner());
+	}
+	
+	private Node createNode(final GetImagingOutcomeType req) throws Exception {
+		JAXBElement<GetImagingOutcomeType> jaxb = objFactory.createGetImagingOutcome(req);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document node = db.newDocument();
+		
+		JAXBContext jc = JAXBContext.newInstance(GetImagingOutcomeType.class);
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.marshal(jaxb, node);
+		return node;
 	}
 }
