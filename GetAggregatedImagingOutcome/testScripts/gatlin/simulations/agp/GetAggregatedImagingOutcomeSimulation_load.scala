31f3048097450d97e953062aa8f9463e65dfb2af
serp <torbjorncla@gmail.com>
2014-11-03 16:22:37 +0100

    Resolved perf-scripts

--- GetAggregatedImagingOutcome/testScripts/gatlin/simulations/agp/GetAggregatedImagingOutcomeSimulation_load.scala
+++ GetAggregatedImagingOutcome/testScripts/gatlin/simulations/agp/GetAggregatedImagingOutcomeSimulation_load.scala
@@ -7,17 +7,17 @@ import com.excilys.ebi.gatling.http.Headers.Names._
 import akka.util.duration._
 import bootstrap._
 
-class GetAggregatedReferralOutcomeSimulation_load extends Simulation {
+class GetAggregatedImagingOutcomeSimulation_load extends Simulation {
 
   val testTimeSecs   = 120
   val noOfUsers      = 10
   val rampUpTimeSecs = 10
-        val minWaitMs      = 2000 milliseconds
+  val minWaitMs      = 2000 milliseconds
   val maxWaitMs      = 5000 milliseconds
 
   // System under test
   val _baseURL        = "http://33.33.33.33:8088"
-  val _contextPath    = "/GetAggregatedReferralOutcome/service/v3"
+  val _contextPath    = "/GetAggregatedImagingOutcome/service/v1"
 
 
 
@@ -28,24 +28,24 @@ class GetAggregatedReferralOutcomeSimulation_load extends Simulation {
   val headers = Map(
     "Accept-Encoding" -> "gzip,deflate",
     "Content-Type" -> "text/xml;charset=UTF-8",
-    "SOAPAction" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetReferralOutcomeResponder:3:GetReferralOutcome",
+    "SOAPAction" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetImagingOutcomeResponder:1:GetImagingOutcome",
     "x-vp-sender-id" -> "sid",
     "x-rivta-original-serviceconsumer-hsaid" -> "TP",
-                "Keep-Alive" -> "115")
+              "Keep-Alive" -> "115")
 
 
-  val scn = scenario("GetAggregatedReferralOutcome")
+  val scn = scenario("GetAggregatedImagingOutcome")
     .during(testTimeSecs) {
       feed(csv("patients.csv").random)
       .exec(
-        http("GetAggregatedReferralOutcome ${patientid} - ${name}")
+        http("GetAggregatedImagingOutcome ${patientid} - ${name}")
           .post(_contextPath)
           .headers(headers)
-          .fileBody("GetReferralOutcome_request", Map("patientId" -> "${patientid}")).asXML
+          .fileBody("GetImagingOutcome_request", Map("patientId" -> "${patientid}")).asXML
           .check(status.is(session => session.getTypedAttribute[String]("status").toInt))
           .check(xpath("soap:Envelope", List("soap" -> "http://schemas.xmlsoap.org/soap/envelope/")).exists)
-          .check(xpath("//*[local-name()='referralOutcome']",
-                         List("ns2" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetReferralOutcomeResponder:3")).count.is(session => session.getTypedAttribute[String]("count").toInt))
+          .check(xpath("//*[local-name()='imagingOutcome']",
+                       List("ns2" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetImagingOutcomeResponder:1")).count.is(session => session.getTypedAttribute[String]("count").toInt))
       )
       .pause(minWaitMs, maxWaitMs)
     }
--- GetAggregatedImagingOutcome/testScripts/gatlin/simulations/agp/GetAggregatedImagingOutcomeSimulation_robustness.scala
+++ GetAggregatedImagingOutcome/testScripts/gatlin/simulations/agp/GetAggregatedImagingOutcomeSimulation_robustness.scala
@@ -7,7 +7,7 @@ import com.excilys.ebi.gatling.http.Headers.Names._
 import akka.util.duration._
 import bootstrap._
 
-class GetAggregatedReferralOutcomeSimulation_load extends Simulation {
+class GetAggregatedImagingOutcomeSimulation_load extends Simulation {
 
   val testTimeSecs   = 43200
   val noOfUsers      = 5
@@ -18,7 +18,7 @@ class GetAggregatedReferralOutcomeSimulation_load extends Simulation {
 
   // System under test
   val _baseURL        = "http://33.33.33.33:8088"
-  val _contextPath    = "/GetAggregatedReferralOutcome/service/v3"
+  val _contextPath    = "/GetAggregatedImagingOutcome/service/v1"
 
 
 
@@ -29,24 +29,24 @@ class GetAggregatedReferralOutcomeSimulation_load extends Simulation {
   val headers = Map(
     "Accept-Encoding" -> "gzip,deflate",
     "Content-Type" -> "text/xml;charset=UTF-8",
-    "SOAPAction" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetReferralOutcomeResponder:3:GetReferralOutcome",
+    "SOAPAction" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetImagingOutcomeResponder:1:GetImagingOutcome",
     "x-vp-sender-id" -> "sid",
     "x-rivta-original-serviceconsumer-hsaid" -> "TP",
                 "Keep-Alive" -> "115")
 
 
-  val scn = scenario("GetAggregatedReferralOutcome")
+  val scn = scenario("GetAggregatedImagingOutcome")
     .during(testTimeSecs) {
       feed(csv("patients.csv").random)
       .exec(
-        http("GetAggregatedReferralOutcome ${patientid} - ${name}")
+        http("GetAggregatedImagingOutcome ${patientid} - ${name}")
           .post(_contextPath)
           .headers(headers)
-          .fileBody("GetReferralOutcome_request", Map("patientId" -> "${patientid}")).asXML
+          .fileBody("GetImagingOutcome_request", Map("patientId" -> "${patientid}")).asXML
           .check(status.is(session => session.getTypedAttribute[String]("status").toInt))
           .check(xpath("soap:Envelope", List("soap" -> "http://schemas.xmlsoap.org/soap/envelope/")).exists)
-          .check(xpath("//*[local-name()='referralOutcome']",
-                         List("ns2" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetReferralOutcomeResponder:3")).count.is(session => session.getTypedAttribute[String]("count").toInt))
+          .check(xpath("//*[local-name()='imagingOutcome']",
+                         List("ns2" -> "urn:riv:clinicalprocess:healthcond:actoutcome:GetImagingOutcomeResponder:1")).count.is(session => session.getTypedAttribute[String]("count").toInt))
       )
       .pause(minWaitMs, maxWaitMs)
     }
